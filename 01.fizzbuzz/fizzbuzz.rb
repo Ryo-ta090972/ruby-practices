fe7b52e860902538f4a8487ce5b3c5ffdd9d47b8
ryo <ta090972@icloud.com>
2023-07-29 00:08:34 +0900

    fizzbuzzの課題提出

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,14 @@
+(1..20).each do |x|
+
+    fizz = (x % 3).zero?
+    buzz = (x % 5).zero?
+
+    if fizz && buzz
+        puts "FizzBuzz"
+    elsif fizz
+        puts "Fizz"
+    elsif buzz
+        puts "Buzz"
+    else puts x
+    end    
+end
