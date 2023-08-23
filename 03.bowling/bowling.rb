1ba8065951a04db92eb3e1d8c758467e64389b63
ryo <ta090972@icloud.com>
2023-08-23 23:46:24 +0900

    配列の変数名を変更、ブロック変数名を変更、frames関連のコード修正、マジックナンバーを定数に設定、total_scoreのループ処理と分岐処理の修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,35 +1,33 @@
 # frozen_string_literal: true
 
+STRIKE = 10
+SPARE = 10
+
 data = ARGV[0]
-down_pin = data.split(',')
+down_pins = data.split(',')
 scores = []
-down_pin.each do |s|
-  if s == 'X'
-    scores << 10
+down_pins.each do |down_pin|
+  if down_pin == 'X'
+    scores << STRIKE
     scores << 0
   else
-    scores << s.to_i
+    scores << down_pin.to_i
   end
 end
 
-frames = []
-scores.each_slice(2) do |s|
-  frames << s
-end
+frames = scores.each_slice(2).to_a
 
 total_score = 0
-frames.each_with_index do |frame, index|
-  next unless index < 10
+10.times do |index|
+  total_score += frames[index].sum
 
-  total_score += if frame[0] == 10 && frames[index + 1][0] == 10
-                   10 + frames[index + 1][0] + frames[index + 2][0]
-                 elsif frame[0] == 10 && frames[index + 1][0] != 10
-                   10 + frames[index + 1].sum
-                 elsif frame.sum == 10
-                   10 + frames[index + 1][0]
-                 else
-                   frame.sum
-                 end
+  if frames[index][0] == STRIKE && frames[index + 1][0] == STRIKE
+    total_score += frames[index + 1][0] + frames[index + 2][0]
+  elsif frames[index][0] == STRIKE && frames[index + 1][0] != STRIKE
+    total_score += frames[index + 1].sum
+  elsif frames[index].sum == SPARE
+    total_score += frames[index + 1][0]
+  end
 end
 
 p total_score
