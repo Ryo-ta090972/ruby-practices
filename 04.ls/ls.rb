c76988d5966ca34275ce64fbf79a6bd33848fbe5
ryo <ta090972@icloud.com>
2023-09-26 11:38:32 +0900

    1.ls.rbは自ファイル実行時のみ処理を実行、2.変数名を一部変更、3.outputメソッドの処理変更、4.max_sizeのtest追加、outputのtest追加

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,21 +1,20 @@
 # frozen_string_literal: true
 
-NUMBER_COLUMN = 3
+COLUMN = 3
 SPACE = 2
-
-path = ARGV[0]
-subject_dir = path || '.'
-flie_and_dir_names = Dir.children(subject_dir)
 
 def not_option(names)
   names.reject { |name| name.start_with?('.') }.sort
 end
 
-def generate(targets, col)
-  row = (targets.size.to_f / col).ceil
-  when_2rows_blank = col - (targets.size - col)
+def generate(targets, column)
+  row = (targets.size.to_f / column).ceil
+  when_2rows_blank = column - (targets.size - column)
   blank_str = when_2rows_blank.positive? ? targets.pop(when_2rows_blank).map { |t| [t] } : []
-  targets.each_slice(row).to_a.push(*blank_str).map! { |t| t.values_at(0...row) }.transpose
+
+  targets.each_slice(row).to_a.push(*blank_str).map! do |t|
+    t.values_at(0...row)
+  end.transpose
 end
 
 def str_max_size_of_each_col(target)
@@ -27,20 +26,32 @@ def str_max_size_of_each_col(target)
       str_size[col] << value.size unless value.nil?
     end
   end
+
   str_size.map(&:max)
 end
 
-def output(target, space)
+def str_for_output(targets, column, space)
+  target = generate(targets, column)
   max_size = str_max_size_of_each_col(target)
 
+  result_lines = []
+
   target.each do |element|
-    element.each_with_index do |value, index|
-      print value.ljust(max_size[index] + space) unless value.nil?
-    end
-    print "\n"
+    line = element.each_with_index.map do |value, col|
+      value.ljust(max_size[col] + space) unless value.nil?
+    end.join.rstrip
+    result_lines << line
   end
+
+  result_lines.join("\n")
 end
 
-targets = not_option(flie_and_dir_names)
-target = generate(targets, NUMBER_COLUMN)
-output(target, SPACE)
+if $PROGRAM_NAME == __FILE__
+
+  path = ARGV[0]
+  subject_dir = path || '.'
+  flie_and_dir_names = Dir.children(subject_dir)
+  targets = not_option(flie_and_dir_names)
+  puts str_for_output(targets, COLUMN, SPACE)
+
+end
--- 04.ls/test/ls_test.rb
+++ 04.ls/test/ls_test.rb
@@ -5,7 +5,7 @@ require_relative '../ls'
 
 class LsTest < Minitest::Test
   def test_not_option
-    assert_equal %w[test1 test2], not_option(['test1', 'test2', '.test3', '.test4'])
+    assert_equal %w[test1 test2], not_option(%w[test1 test2 .test3 .test4])
   end
 
   def test_generate_val1_row1_col3
@@ -41,6 +41,39 @@ class LsTest < Minitest::Test
   def test_generate_val10_row4_col3
     result = generate((1..10).to_a, 3)
     expected = [[1, 5, 9], [2, 6, 10], [3, 7, nil], [4, 8, nil]]
+    assert_equal expected, result
+  end
+
+  def test_str_max_size_of_each_col_val1
+    result = str_max_size_of_each_col([['1']])
+    expected = [1]
+    assert_equal expected, result
+  end
+
+  def test_str_max_size_of_each_col_val6_col3
+    result = str_max_size_of_each_col([%w[1 22 666666], %w[333 4444 nil]])
+    expected = [3, 4, 6]
+    assert_equal expected, result
+  end
+
+  def test_str_for_output_val4_col3_space2
+    targets = %w[test1 test2 test3 test4]
+    result = str_for_output(targets, 3, 2)
+    expected = <<~TEXT.chomp
+      test1  test3  test4
+      test2
+    TEXT
+    assert_equal expected, result
+  end
+
+  def test_str_for_output_val7_col3_space3
+    targets = %w[test1 test2 test3test3 test4 test5 test6 test7]
+    result = str_for_output(targets, 3, 3)
+    expected = <<~TEXT.chomp
+      test1        test4   test7
+      test2        test5
+      test3test3   test6
+    TEXT
     assert_equal expected, result
   end
 end
