71b341d9e803b512ddbec0b4dc6f8e538e732311
ryo <ta090972@icloud.com>
2023-10-25 13:51:41 +0900

    1.sort_namesメソッドの処理内容を変更 2.-lオプションの追加 3.try_load_attributesメソッドで詳細情報を取得 4.変数名、メソッド名を変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,17 +1,20 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'etc'
 
 COLUMN = 3
-SPACE = 2
+FORMAT_TIME = '%b %d %H:%M'
+BLOCK_SIZA = 1024
 
 def main
   parsed_options = parse_options
-  target_dir = ARGV.empty? ? '.' : ARGV[0]
-  entry_names = Dir.entries(target_dir)
-  sorted_names = sort_names(entry_names, parsed_options)
+  target_dir_path = ARGV.empty? ? '.' : ARGV[0]
+  names = Dir.entries(target_dir_path)
+  sorted_names = sort_names(names, parsed_options)
   filtered_names = filter_names(sorted_names, parsed_options)
-  puts output(filtered_names)
+  texts = try_load_attributes(filtered_names, parsed_options, target_dir_path)
+  puts format_texts_layout(texts, parsed_options)
 end
 
 def parse_options
@@ -19,46 +22,132 @@ def parse_options
   OptionParser.new do |opts|
     opts.on('-a') { options[:a] = true }
     opts.on('-r') { options[:r] = true }
+    opts.on('-l') { options[:l] = true }
   end.parse!(ARGV)
   options
 end
 
 def sort_names(names, options)
-  options[:r] ? names.sort.reverse : names.sort
+  sorted_names = names.sort_by(&:upcase)
+  options[:r] ? sorted_names.reverse : sorted_names
 end
 
 def filter_names(names, options)
   options[:a] ? names : names.reject { |name| name.start_with?('.') }
 end
 
-def output(names)
-  repositioned_names = reposition(names)
-  max_str_sizes = find_max_str_sizes(repositioned_names)
+def try_load_attributes(names, options, path)
+  return names unless options[:l]
 
-  repositioned_names.each.map do |names_for_row|
-    names_for_row.each_with_index.map do |name, col|
-      name.to_s.ljust(max_str_sizes[col] + SPACE)
+  total_block_size = 0
+
+  names.each.map do |name|
+    name_path = File.absolute_path(name, path)
+    name_attribute = File::Stat.new(name_path)
+    total_block_size += name_attribute.blksize.to_i / BLOCK_SIZA
+    attributes = []
+
+    attributes << load_type_and_permission(name_attribute)
+    attributes << name_attribute.nlink
+    attributes << Etc.getpwuid(name_attribute.uid).name
+    attributes << Etc.getpwuid(name_attribute.gid).name
+    attributes << name_attribute.size
+    attributes << name_attribute.mtime.strftime(FORMAT_TIME)
+    attributes << name
+  end.prepend(["total #{total_block_size}"])
+end
+
+def load_type_and_permission(name)
+  number = name.mode.to_s(8).rjust(6, '0')
+  type = to_type(number[0, 2])
+  permission = to_premission(number[3, 3])
+  tried_authority_permission = try_to_authority(permission, number[2, 1])
+  type + tried_authority_permission
+end
+
+def to_type(number)
+  {
+    '01' => 'p',
+    '02' => 'c',
+    '04' => 'd',
+    '10' => '-',
+    '12' => 'l',
+    '14' => 's'
+  }[number]
+end
+
+def to_premission(numbers)
+  numbers.each_char.map do |number|
+    {
+      '0' => '---',
+      '1' => '--x',
+      '2' => '-w-',
+      '3' => '-wx',
+      '4' => 'r--',
+      '5' => 'r-x',
+      '6' => 'rw-',
+      '7' => 'rwx'
+    }[number]
+  end.join
+end
+
+def try_to_authority(permission, number)
+  return permission if number == '0'
+
+  index = {
+    '1' => 8,
+    '2' => 5,
+    '4' => 2
+  }[number]
+
+  authority = {
+    '1' => { 'x' => 't', '-' => 'T' },
+    '2' => { 'x' => 's', '-' => 'S' },
+    '4' => { 'x' => 's', '-' => 'S' }
+  }[number]
+
+  permission[index] = authority[permission[index]]
+  permission
+end
+
+def format_texts_layout(texts, options)
+  tyied_reposition_texts = try_reposition(texts, options)
+  max_str_sizes = find_max_str_sizes(tyied_reposition_texts)
+
+  tyied_reposition_texts.each.map do |texts_for_row|
+    texts_for_row.each_with_index.map do |text, col|
+      if options[:l] && !array_last?(texts_for_row, col)
+        "#{text.to_s.rjust(max_str_sizes[col])} "
+      else
+        "#{text.to_s.ljust(max_str_sizes[col])}  "
+      end
     end.join.rstrip
   end.join("\n")
 end
 
-def reposition(names)
-  row = (names.size.to_f / COLUMN).ceil
-  names.each_slice(row).map do |names_for_row|
-    names_for_row.values_at(0...row)
+def try_reposition(texts, options)
+  return texts if options[:l]
+
+  row = (texts.size.to_f / COLUMN).ceil
+  texts.each_slice(row).map do |texts_for_row|
+    texts_for_row.values_at(0...row)
   end.transpose
 end
 
-def find_max_str_sizes(nested_names)
+def find_max_str_sizes(nested_texts)
   str_sizes = []
-  nested_names.each do |names|
-    names.each_with_index do |name, col|
+  nested_texts.each do |texts|
+    texts.each_with_index do |text, col|
       str_sizes[col] ||= []
-      str_sizes[col] << name.to_s.size
+      str_sizes[col] << text.to_s.size
     end
   end
 
   str_sizes.map(&:max)
+end
+
+def array_last?(array, index)
+  array[index] == array.last
 end
 
 main if $PROGRAM_NAME == __FILE__
--- 04.ls/test/ls_test.rb
+++ 04.ls/test/ls_test.rb
@@ -4,40 +4,58 @@ require 'minitest/autorun'
 require_relative '../ls'
 
 class LsTest < Minitest::Test
-  def test_filter_names_not_option
-    options = {}
-    result = filter_names(%w[test1 test2 .test3 .test4], options)
+  def test_filter_names_not_option_a
+    option = { a: false }
+    result = filter_names(%w[test1 test2 .test3 .test4], option)
     expected = %w[test1 test2]
     assert_equal expected, result
   end
 
   def test_filter_names_option_a
-    options = { a: true }
-    result = filter_names(%w[. .. test1 test2 .test3], options)
+    option = { a: true }
+    result = filter_names(%w[. .. test1 test2 .test3], option)
     expected = %w[. .. test1 test2 .test3]
     assert_equal expected, result
   end
 
+  def test_sort_names_not_option_r
+    option = { r: false }
+    result = sort_names(%w[B a C d], option)
+    expected = %w[a B C d]
+    assert_equal expected, result
+  end
+
+  def test_sort_names_option_r
+    option = { r: true }
+    result = sort_names(%w[A b D c], option)
+    expected = %w[D c b A]
+    assert_equal expected, result
+  end
+
   def test_reposition_val1_row1
-    result = reposition([1])
+    option = { l: false }
+    result = try_reposition([1], option)
     expected = [[1]]
     assert_equal expected, result
   end
 
   def test_reposition_val3_row1_col3
-    result = reposition((1..3).to_a)
+    option = { l: false }
+    result = try_reposition((1..3).to_a, option)
     expected = [[1, 2, 3]]
     assert_equal expected, result
   end
 
   def test_reposition_val4_row2_col3
-    result = reposition((1..4).to_a)
+    option = { l: false }
+    result = try_reposition((1..4).to_a, option)
     expected = [[1, 3], [2, 4]]
     assert_equal expected, result
   end
 
   def test_reposition_val10_row4_col3
-    result = reposition((1..10).to_a)
+    option = { l: false }
+    result = try_reposition((1..10).to_a, option)
     expected = [[1, 5, 9], [2, 6, 10], [3, 7, nil], [4, 8, nil]]
     assert_equal expected, result
   end
@@ -54,9 +72,10 @@ class LsTest < Minitest::Test
     assert_equal expected, result
   end
 
-  def test_output_val4
+  def test_format_texts_layout_val4
+    option = { l: false }
     names = %w[test1 test2 test3 test4]
-    result = output(names)
+    result = format_texts_layout(names, option)
     expected = <<~TEXT.chomp
       test1  test3
       test2  test4
@@ -64,14 +83,28 @@ class LsTest < Minitest::Test
     assert_equal expected, result
   end
 
-  def test_output_val7
+  def test_format_texts_layout_val7
+    option = { l: false }
     names = %w[test1 test2 test3test3 test4 test5 test6 test7]
-    result = output(names)
+    result = format_texts_layout(names, option)
     expected = <<~TEXT.chomp
       test1       test4  test7
       test2       test5
       test3test3  test6
     TEXT
+    assert_equal expected, result
+  end
+
+  def test_format_texts_layout_option_l
+    options = { a: false, r: false, l: true }
+    target_dir_path = '.'
+    names = Dir.entries(target_dir_path)
+    sorted_names = sort_names(names, options)
+    filtered_names = filter_names(sorted_names, options)
+    texts = try_load_attributes(filtered_names, options, target_dir_path)
+
+    result = format_texts_layout(texts, options)
+    expected = `ls -l`.chomp.gsub('\n', ' ')
     assert_equal expected, result
   end
 end
