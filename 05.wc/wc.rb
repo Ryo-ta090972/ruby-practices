837d828b6748e0bb35fad622769c9fdcdde19c3a
ryo <ta090972@icloud.com>
2023-11-25 23:00:13 +0900

    1.具体的なメソッド名を抽象化して短縮 2.parse_optionsメソッドの処理内容を変更 3.定数WIDTH_xxxを削除 4.widthは最大文字列で設定

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -3,28 +3,32 @@
 require 'optparse'
 require 'debug'
 
-WIDTH_FOR_RJUST_0 = 0
-WIDTH_FOR_RJUST_7 = 7
+TARGET_KEY = %w[row word byte].freeze
 
 def main
   options = parse_options
-  loaded_files_attributes = load_files_attributes
-  output_text = to_output_text(loaded_files_attributes, options)
+  loaded_texts_attributes = load_texts_attributes
+  output_text = to_output_text(loaded_texts_attributes, options)
   puts output_text
 end
 
 def parse_options
-  options = { row: true, word: true, byte: true }
+  options = { row: nil, word: nil, byte: nil, name: nil }
 
   OptionParser.new do |opts|
-    opts.on('-l') { options[:row] = false }
-    opts.on('-w') { options[:word] = false }
-    opts.on('-c') { options[:byte] = false }
+    opts.on('-l') { options[:row] = true, options[:name] = true }
+    opts.on('-w') { options[:word] = true, options[:name] = true }
+    opts.on('-c') { options[:byte] = true, options[:name] = true }
   end.parse!(ARGV)
-  options
+
+  if options.values.none?
+    options.transform_values { true }
+  else
+    options.compact
+  end
 end
 
-def load_files_attributes
+def load_texts_attributes
   if ARGV.empty?
     text = $stdin.read
     [calculate_attributes(text)]
@@ -47,19 +51,19 @@ def calculate_attributes(text, file_name = nil)
   }
 end
 
-def to_output_text(files_attributes, options)
-  copy_files_attributes = files_attributes.dup
-  copy_files_attributes << build_total_attributes(copy_files_attributes) if multiple_files?(copy_files_attributes)
-  width_for_rjust = calculate_width_for_rjust(copy_files_attributes, options)
-  output_files_attributes = options.all? { |_, bool| bool } ? copy_files_attributes : delete_attribute_by_option(copy_files_attributes, options)
-  format_attributes(output_files_attributes, width_for_rjust)
+def to_output_text(texts_attributes, options)
+  copy_texts_attributes = texts_attributes.dup
+  copy_texts_attributes << build_total(copy_texts_attributes) if multiple_texts?(copy_texts_attributes)
+  width_for_rjust = calculate_max_length(copy_texts_attributes)
+  output_texts = delete_attribute(copy_texts_attributes, options)
+  format_texts(output_texts, width_for_rjust)
 end
 
-def multiple_files?(files)
-  files.length > 1
+def multiple_texts?(texts)
+  texts.length > 1
 end
 
-def build_total_attributes(attributes)
+def build_total(attributes)
   total_row = attributes.sum { |attribute| attribute[:row] }
   total_word = attributes.sum { |attribute| attribute[:word] }
   total_byte = attributes.sum { |attribute| attribute[:byte] }
@@ -72,34 +76,24 @@ def build_total_attributes(attributes)
   }
 end
 
-def calculate_width_for_rjust(files, options)
-  if ARGV.empty?
-    options.one? { |_, bool| !bool } ? WIDTH_FOR_RJUST_0 : WIDTH_FOR_RJUST_7
-  else
-    options.one? { |_, bool| !bool } && !multiple_files?(files) ? WIDTH_FOR_RJUST_0 : calculate_max_int_length_whole_attributes(files)
-  end
-end
-
-def calculate_max_int_length_whole_attributes(files)
-  files.map do |attributes|
-    attributes.values.map do |attribute|
-      attribute.to_s.length if attribute.instance_of?(Integer)
+def calculate_max_length(texts)
+  texts.map do |attributes|
+    attributes.map do |key, attribute|
+      attribute.to_s.length if TARGET_KEY.include?(key.to_s)
     end.compact
   end.flatten.max
 end
 
-def delete_attribute_by_option(files, options)
-  files.map do |attributes|
-    attributes.reject { |key, _| options[key] }
+def delete_attribute(texts, options)
+  texts.map do |attributes|
+    attributes.select { |key, _| options[key] }
   end
 end
 
-def format_attributes(files, width)
-  target_key_for_rjust = %w[row word byte]
-
-  files.map do |attributes|
+def format_texts(texts, width)
+  texts.map do |attributes|
     attributes.map do |key, attribute|
-      target_key_for_rjust.include?(key.to_s) ? "#{attribute.to_s.rjust(width)} " : attribute
+      TARGET_KEY.include?(key.to_s) ? "#{attribute.to_s.rjust(width)} " : attribute
     end.join.rstrip
   end.join("\n")
 end
