4ea8d67fe2c92ed83abb6ffe2bb28793c736d627
ryo <ta090972@icloud.com>
2024-04-08 17:57:44 +0900

    mainクラス作成

---
+++ 06.bowling_object/main.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Main
+  attr_reader :frames
+
+  def initialize(scores = ARGV[0])
+    @scores = scores.split(',')
+    @frames = []
+  end
+
+  def build_frames
+    @scores.each do |score|
+      if score == 'X'
+        @frames << score
+        @frames << '0'
+      else
+        @frames << score
+      end
+    end
+  end
+end
