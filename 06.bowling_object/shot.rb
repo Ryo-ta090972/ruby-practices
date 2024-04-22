ee5ae18a4f4f11e02430869a1aeb8113c12f31bb
ryo <ta090972@icloud.com>
2024-04-22 13:40:46 +0900

    メソッド追加（strike?）、定数設定（STRIKE）：shot.rb

--- 06.bowling_object/shot.rb
+++ 06.bowling_object/shot.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 class Shot
+  STRIKE = 'X'
+  
   def initialize(mark)
     @mark = mark
   end
 
   def score
-    @mark == 'X' ? 10 : @mark.to_i
+    @mark == STRIKE ? 10 : @mark.to_i
+  end
+
+  def strike?
+    @mark == STRIKE
   end
 end
