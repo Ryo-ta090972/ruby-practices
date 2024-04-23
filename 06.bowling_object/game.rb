c11db7699c3029bc6361413445e8468cfc454980
ryo <ta090972@icloud.com>
2024-04-23 11:26:26 +0900

    scoreメソッド修正：game.rb

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -9,38 +9,20 @@ class Game
 
   def score
     frames = build_frames
-    calculate_normal_score(frames) + calculate_bonus_score(frames)
+
+    frames.take(10).sum do |frame|
+      frame.calculate_score(frames)
+    end
   end
 
   private
 
   def build_frames
-    build_shots.each_slice(2).map { |shots| Frame.new(shots) }
+    build_shots.each_slice(2).each_with_index.map { |shots, index| Frame.new(shots, index) }
   end
 
   def build_shots
     shots = @marks.map { |mark| Shot.new(mark) }
     shots.flat_map { |shot| shot.strike? ? [shot, Shot.new('0')] : shot }
-  end
-
-  def calculate_normal_score(frames)
-    frames.take(10).sum(&:score)
-  end
-
-  def calculate_bonus_score(frames)
-    frames.take(10).each.with_index.sum do |frame, index|
-      next_frame = frames[index + 1]
-      next_next_frame = frames[index + 2]
-
-      if frame.strike? && next_frame.strike?
-        next_frame.score + next_next_frame.first_shot_score
-      elsif frame.strike? && !next_frame.strike?
-        next_frame.score
-      elsif frame.spare?
-        next_frame.first_shot_score
-      else
-        0
-      end
-    end
   end
 end
