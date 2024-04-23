0f64d6563ceca7e10042dd5204e7cbdf665ca673
ryo <ta090972@icloud.com>
2024-04-23 11:28:02 +0900

    インスタンス変数（@index）追加、メソッド変更、calculate_bonus_scorメソッド追加：frame.rb

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -3,11 +3,16 @@
 require './shot'
 
 class Frame
-  def initialize(shots)
+  def initialize(shots, index)
     @shots = shots
+    @index = index
   end
 
-  def score
+  def calculate_score(frames)
+    normal_score + calculate_bonus_score(frames)
+  end
+
+  def normal_score
     @shots.sum(&:score)
   end
 
@@ -16,10 +21,27 @@ class Frame
   end
 
   def spare?
-    !strike? && score == 10
+    !strike? && normal_score == 10
   end
 
   def first_shot_score
     @shots[0].score
+  end
+
+  private
+
+  def calculate_bonus_score(frames)
+    next_frame = frames[@index + 1]
+    next_next_frame = frames[@index + 2]
+
+    if strike? && next_frame.strike?
+      next_frame.first_shot_score + next_next_frame.first_shot_score
+    elsif strike? && !next_frame.strike?
+      next_frame.normal_score
+    elsif spare?
+      next_frame.first_shot_score
+    else
+      0
+    end
   end
 end
