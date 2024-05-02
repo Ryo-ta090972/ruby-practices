83c791136a77e93cb18e627e5c6ca8f805cb2639
ryo <ta090972@icloud.com>
2024-05-02 15:50:49 +0900

    commandクラスのインスタンス変数をcommand_lineのみにする

--- 07.ls_object/lib/command.rb
+++ 07.ls_object/lib/command.rb
@@ -1,16 +1,22 @@
 # frozen_string_literal: true
 
 require 'optparse'
-require_relative '../lib/command_line'
+require './lib/command_line'
 
 class Command
   def initialize(argv = ARGV)
-    command_line = CommandLine.new(argv)
-    @options = command_line.options
-    @paths = command_line.paths
+    @command_line = CommandLine.new(argv)
   end
 
   def ls
-    @paths.map {|path| Dir.entries(path)}
+    paths.map {|path| Dir.entries(path)}
+  end
+
+  def option
+    @command_line.option
+  end
+
+  def paths
+    @command_line.paths
   end
 end
