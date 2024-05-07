979e0c014828a83ada3fd8969144409b9b8699ad
ryo <ta090972@icloud.com>
2024-05-07 17:31:21 +0900

    出力用のテキストでテストを行うよう修正：command_test.rb

--- 07.ls_object/test/command_test.rb
+++ 07.ls_object/test/command_test.rb
@@ -6,41 +6,61 @@ require 'debug'
 
 class CommandTest < Test::Unit::TestCase
   def test_ls_for_path
-    # binding.break
-    argv = []
+    argv = ['.']
     result = Command.new(argv).ls
-    expected = [["TODO.txt", "lib", "test"]]
+    expected = <<~TEXT.chomp
+    TODO.txt                lib                     test
+    TEXT
+    
     assert_equal(expected, result)
   end
 
   def test_ls_for_paths
     argv = ['.', './test']
     result = Command.new(argv).ls
-    expected = [["TODO.txt", "lib", "test"], ["command_test.rb"]]
+    expected = <<~TEXT.chomp
+    .:
+    TODO.txt                lib                     test
+    
+    ./test:
+    command_test.rb
+    TEXT
+
     assert_equal(expected, result)
   end
 
   def test_ls_option_all
     argv = ['-a']
     result = Command.new(argv).ls
-    expected = [[".", "..", ".gitignore", ".gitkeep", ".vscode", "TODO.txt", "lib", "test"]]
+    expected = <<~TEXT.chomp
+    .                       .gitkeep                lib
+    ..                      .vscode                 test
+    .gitignore              TODO.txt
+    TEXT
+    
     assert_equal(expected, result)
   end
 
   def test_ls_option_reverse
     argv = ['-r']
     result = Command.new(argv).ls
-    expected = [["test", "lib", "TODO.txt"]]
+    expected = <<~TEXT.chomp
+    test                    lib                     TODO.txt
+    TEXT
+
     assert_equal(expected, result)
   end
 
   def test_ls_option_long
     argv = ['-l']
     result = Command.new(argv).ls
-    expected = [[["total 16"],
-    ["-rw-r--r--", 1, "ryo", "staff", 4376, "5  1 23:30", "TODO.txt"],
-    ["drwxr-xr-x", 10, "ryo", "staff", 320, "5  2 22:57", "lib"],
-    ["drwxr-xr-x", 3, "ryo", "staff", 96, "5  1 23:29", "test"]]]
+    expected = <<~TEXT.chomp
+    total 16
+    -rw-r--r--@  1 ryo  staff  4376  5  1 23:30 TODO.txt
+    drwxr-xr-x  14 ryo  staff   448  5  7 13:31 lib
+    drwxr-xr-x   3 ryo  staff    96  5  7 15:06 test
+    TEXT
+    
     assert_equal(expected, result)
   end
 end
