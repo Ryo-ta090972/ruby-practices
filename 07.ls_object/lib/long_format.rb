481ec32bac8e0195e73fbf9998fa0330e9491858
ryo <ta090972@icloud.com>
2024-05-14 16:53:05 +0900

    Timeクラスのフォーマットを追加、出力用の文字列変換メソッドを追加

--- 07.ls_object/lib/long_format.rb
+++ 07.ls_object/lib/long_format.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 require 'debug'
-class LongFormat < ShortFormat
+class LongFormat
   def initialize(entry_groups)
     @entry_groups = entry_groups
   end
@@ -30,11 +30,36 @@ class LongFormat < ShortFormat
         entries.each_with_index.map do |entry, column|
           if entry.instance_of?(Integer)
             "#{entry.to_s.rjust(max_sizes[path][column])} "
+          elsif entry.instance_of?(Time)
+            "#{entry.strftime('%_m %e %H:%M')} "
+          elsif last?(entries, column)
+            entry
           else
-            "#{entry.to_s.ljust(max_sizes[path][column])} "
+            "#{entry.to_s.ljust(max_sizes[path][column])}  "
           end
         end
       end
     end
+  end
+
+  def convert_output_string(entry_groups)
+    entry_groups.each_with_object([]) do |(path, group_entries), output_strings|
+      output_strings << "#{path}:\n" if !entry_groups.one?
+      
+      group_entries.each do |entries|
+        entries.each_with_index do |entry, index|
+          if last?(entries, index)
+            output_strings << "#{entry}\n"
+          else
+            output_strings << "#{entry.to_s}"
+          end
+        end
+      end
+      output_strings << "\n"
+    end.join.rstrip
+  end
+
+  def last?(entries, index)
+    index == entries.size - 1
   end
 end
