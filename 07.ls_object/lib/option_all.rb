22f70ba3dae803cb899adb5b8a28df1e95051521
ryo <ta090972@icloud.com>
2024-05-28 19:08:34 +0900

    不要クラスを削除

--- 07.ls_object/lib/option_all.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require './lib/option_base'
-
-class OptionAll < OptionBase
-  def apply(_, path)
-    Dir.entries(path).sort
-  end
-end
--- 07.ls_object/lib/option_reverse.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require './lib/option_base'
-
-class OptionReverse < OptionBase
-  def apply(entries, _)
-    entries.sort.reverse
-  end
-end
