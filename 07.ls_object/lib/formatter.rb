faabe20ffa9557ba5ef4840342e8dd2f4b324da3
ryo <ta090972@icloud.com>
2024-05-30 16:56:52 +0900

    long_formatメソッドを中間状態となる配列を作らずにフォーマットも含めて一括処理するように変更、クラス名変更に伴う修正漏れを修正

--- 07.ls_object/lib/formatter.rb
+++ 07.ls_object/lib/formatter.rb
@@ -11,106 +11,66 @@ class Formatter
   end
 
   def format(directory, paths)
-    target_entries = @show_all ? directory.entries : directory.entries.reject { |entry| entry.name.start_with?('.') }
-    sorted_entries = @reverse_order ? target_entries.reverse : target_entries
-    formatter_entries = @show_long_format ? long_format(sorted_entries) : short_format(sorted_entries)
-    multiple?(paths) ? ["#{directory.path}:", formatter_entries].join("\n") : formatter_entries
+    target_files = @show_all ? directory.files : directory.files.reject { |file| file.name.start_with?('.') }
+    sorted_files = @reverse_order ? target_files.reverse : target_files
+    max_sizes = directory.find_max_size_of_file_details(sorted_files)
+    formatter_files = @show_long_format ? long_format(sorted_files, max_sizes) : short_format(sorted_files, max_sizes)
+    multiple?(paths) ? ["#{directory.path}:", formatter_files].join("\n") : formatter_files
   end
 
   private
 
-  def long_format(entries)
-    attributes = build_attributes(entries)
-    total_block_size = entries.sum(&:block_size)
-    format_attributes(attributes, total_block_size)
+  def long_format(files, max_sizes)
+    file_details = build_file_details(files, max_sizes)
+    total_block_size = files.sum(&:block_size)
+    ["total #{total_block_size}", file_details].join("\n")
   end
 
-  def build_attributes(entries)
-    entries.map do |entry|
+  def build_file_details(files, max_sizes)
+    files.map do |file|
       [
-        entry.type + entry.permission,
-        entry.nlink,
-        entry.uid,
-        entry.gid,
-        entry.size,
-        entry.mtime,
-        entry.name
-      ]
-    end
-  end
-
-  def format_attributes(rows_attributes, total_block_size)
-    head = "total #{total_block_size}"
-    body = render_long_format_body(rows_attributes)
-    [head, body].join("\n")
-  end
-
-  def render_long_format_body(rows_attributes)
-    max_sizes = find_max_attribute_sizes(rows_attributes)
-
-    rows_attributes.map do |row_attributes|
-      row_attributes.each_with_index.map do |attribute, column|
-        max_size = max_sizes[column]
-
-        if last?(row_attributes, column)
-          "#{attribute}\n"
-        elsif attribute.instance_of?(Integer)
-          "#{attribute.to_s.rjust(max_size)} "
-        elsif attribute.instance_of?(Time)
-          "#{attribute.strftime('%_m %e %H:%M')} "
-        else
-          "#{attribute.to_s.ljust(max_size)}  "
-        end
-      end.join
+        file.type_and_permission.ljust(max_sizes[:type_and_permission] + 1),
+        file.nlink.to_s.rjust(max_sizes[:nlink]),
+        file.uid.ljust(max_sizes[:uid] + 1),
+        file.gid.ljust(max_sizes[:gid] + 1),
+        file.size.to_s.rjust(max_sizes[:size]),
+        file.mtime.strftime('%_m %e %H:%M'),
+        "#{file.name}\n"
+      ].join(" ")
     end.join
   end
 
-  def find_max_attribute_sizes(rows_attributes)
-    rows_attributes.transpose.map do |row_attributes|
-      row_attributes.map do |attribute|
-        attribute.to_s.size
-      end.max
-    end
+  def short_format(files, max_sizes)
+    file_names = files.map(&:name)
+    row = cal_row(file_names)
+    max_size = max_sizes[:name]
+    width = cal_width(max_size)
+    transposed_file_names = transpose_file_names(file_names, row)
+    format_names(transposed_file_names, width)
   end
 
-  def short_format(entries)
-    names = entries.map(&:name)
-    row = cal_row(names)
-    width = cal_width(names)
-    transposed_names = transpose_names(names, row)
-    format_names(transposed_names, width)
+  def cal_row(file_names)
+    (file_names.size.to_f / COLUMN).ceil
   end
 
-  def cal_row(names)
-    (names.size.to_f / COLUMN).ceil
-  end
-
-  def cal_width(names)
-    max_string_size = find_max_name_size(names)
-
-    if (max_string_size % SPACE).zero?
-      (max_string_size.to_f / SPACE + 1).ceil * SPACE
+  def cal_width(max_size)
+    if (max_size % SPACE).zero?
+      (max_size.to_f / SPACE + 1).ceil * SPACE
     else
-      (max_string_size.to_f / SPACE).ceil * SPACE
+      (max_size.to_f / SPACE).ceil * SPACE
     end
   end
 
-  def find_max_name_size(names)
-    names.map do |name|
-      name.to_s.size
-    end.max
-  end
-
-  def transpose_names(names, row)
-    names.each_slice(row).map do |sliced_names|
-      sliced_names.values_at(0...row)
+  def transpose_file_names(file_names, row)
+    file_names.each_slice(row).map do |sliced_file_names|
+      sliced_file_names.values_at(0...row)
     end.transpose
   end
 
-  def format_names(rows_names, width)
-    rows_names.map do |row_names|
-      row_names.each_with_index.map do |name, column|
-        last?(row_names, column) ? "#{name}\n" : name.to_s.ljust(width)
+  def format_names(rows_file_names, width)
+    rows_file_names.map do |row_file_names|
+      row_file_names.each_with_index.map do |file_name, column|
+        last?(row_file_names, column) ? "#{file_name.to_s}\n" : file_name.to_s.ljust(width)
       end.join
     end.join
   end
