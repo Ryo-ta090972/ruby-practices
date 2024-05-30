82d6ea6e2d1815e68175d346afbf0d93475e9ae4
ryo <ta090972@icloud.com>
2024-05-30 12:50:32 +0900

    メソッド名を変更

--- 07.ls_object/lib/ls.rb
+++ 07.ls_object/lib/ls.rb
@@ -2,4 +2,4 @@
 
 require_relative 'ls_command'
 
-print LsCommand.output
+print LsCommand.execute
--- 07.ls_object/lib/ls_command.rb
+++ 07.ls_object/lib/ls_command.rb
@@ -5,14 +5,14 @@ require_relative 'directory'
 require_relative 'formatter'
 
 class LsCommand
-  def self.output
+  def self.execute
     command_line = CommandLine.new
     options = command_line.options
     paths = command_line.paths
-    new.output(options, paths)
+    new.render(options, paths)
   end
 
-  def output(options, paths)
+  def render(options, paths)
     directories = paths.map { |path| Directory.new(path) }
     formatter = Formatter.new(options)
 
