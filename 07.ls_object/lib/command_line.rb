5fe3993b3d25a65a5e8c032d95e32c4edef627b2
ryo <ta090972@icloud.com>
2024-05-30 12:32:49 +0900

    破壊的なメソッドとわかるようなメソッド名に変更

--- 07.ls_object/lib/command_line.rb
+++ 07.ls_object/lib/command_line.rb
@@ -6,13 +6,13 @@ class CommandLine
   attr_reader :options, :paths
 
   def initialize(argv = ARGV)
-    @options = parse_options(argv)
+    @options = parse_options!(argv)
     @paths = argv.empty? ? ['.'] : argv.sort
   end
 
   private
 
-  def parse_options(argv)
+  def parse_options!(argv)
     options = {}
     OptionParser.new do |opts|
       opts.on('-a') { options[:all] = true }
