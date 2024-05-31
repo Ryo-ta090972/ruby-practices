dd3a8614c237e0722e942f7273c6910868172b3c
ryo <ta090972@icloud.com>
2024-05-31 19:13:40 +0900

    メソッド名変更に伴いテストコードを修正

--- 07.ls_object/test/ls_command_test.rb
+++ 07.ls_object/test/ls_command_test.rb
@@ -11,7 +11,7 @@ class LsCommandTest < Test::Unit::TestCase
 
   def test_ls_when_single_path
     paths = ['./test/sample_dir']
-    result = @ls_command.render(@options, paths)
+    result = @ls_command.generate(@options, paths)
     expected = <<~TEXT
       Ellen.txt       bob.txt         gim
       Frank.txt       carol.rb        
@@ -23,7 +23,7 @@ class LsCommandTest < Test::Unit::TestCase
 
   def test_ls_when_multiple_paths
     paths = ['./test/sample_dir', './test/sample_dir/alice']
-    result = @ls_command.render(@options, paths)
+    result = @ls_command.generate(@options, paths)
     expected = <<~TEXT
       ./test/sample_dir:
       Ellen.txt       bob.txt         gim
@@ -40,7 +40,7 @@ class LsCommandTest < Test::Unit::TestCase
   def test_ls_when_single_path_and_option_all
     @options = { all: true }
     paths = ['./test/sample_dir']
-    result = @ls_command.render(@options, paths)
+    result = @ls_command.generate(@options, paths)
     expected = <<~TEXT
       .                       .alice_hidden           alice
       ..                      .bob_hidden.txt         bob.txt
@@ -55,7 +55,7 @@ class LsCommandTest < Test::Unit::TestCase
   def test_ls_when_single_path_and_option_reverse
     @options = { reverse: true }
     paths = ['./test/sample_dir']
-    result = @ls_command.render(@options, paths)
+    result = @ls_command.generate(@options, paths)
     expected = <<~TEXT
       gim             bob.txt         Ellen.txt
       dave.js         alice           
@@ -68,7 +68,7 @@ class LsCommandTest < Test::Unit::TestCase
   def test_ls_when_single_path_and_option_long
     @options = { long: true }
     paths = ['./test/sample_dir']
-    result = @ls_command.render(@options, paths)
+    result = @ls_command.generate(@options, paths)
     expected = <<~TEXT
     total 0
     -rw-r--r--  1 ryo  staff   0  5  7 18:09 Ellen.txt
@@ -86,31 +86,31 @@ class LsCommandTest < Test::Unit::TestCase
   def test_ls_when_multiple_paths_and_option_all_long
     @options = { all: true, long: true }
     paths = ['./test/sample_dir', './test/sample_dir/alice']
-    result = @ls_command.render(@options, paths)
+    result = @ls_command.generate(@options, paths)
     expected = <<~TEXT
-    ./test/sample_dir:
-    total 0
-    drwxr-xr-x  15 ryo  staff  480  5 30 15:59 .
-    drwxr-xr-x   4 ryo  staff  128  5 30 13:18 ..
-    -rw-r--r--   1 ryo  staff    0  5  7 18:11 .Carol_hidden.rb
-    -rw-r--r--   1 ryo  staff    0  5  7 18:11 .Dave_hidden.js
-    -rw-r--r--   1 ryo  staff    0  5  7 18:11 .Frank_hidden.txt
-    -rw-r--r--   1 ryo  staff    0  5  7 18:10 .alice_hidden
-    -rw-r--r--   1 ryo  staff    0  5  7 18:10 .bob_hidden.txt
-    -rw-r--r--   1 ryo  staff    0  5  7 18:11 .ellen_hidden.txt
-    -rw-r--r--   1 ryo  staff    0  5  7 18:09 Ellen.txt
-    -rw-r--r--   1 ryo  staff    0  5  7 18:10 Frank.txt
-    drwxr-xr-x   3 ryo  staff   96  5 30 15:59 alice
-    -rw-r-Sr--   1 ryo  staff    0  5  7 18:08 bob.txt
-    -rw-r--r--   1 ryo  staff    0  5  7 18:08 carol.rb
-    -rw-r--r--   1 ryo  staff    0  5  7 18:08 dave.js
-    drwxr-xr-t   2 ryo  staff   64  5  7 18:20 gim
-    
-    ./test/sample_dir/alice:
-    total 0
-    drwxr-xr-x   3 ryo  staff   96  5 30 15:59 .
-    drwxr-xr-x  15 ryo  staff  480  5 30 15:59 ..
-    -rw-r--r--   1 ryo  staff    0  5 30 15:59 alice.txt
+      ./test/sample_dir:
+      total 0
+      drwxr-xr-x  15 ryo  staff  480  5 30 15:59 .
+      drwxr-xr-x   4 ryo  staff  128  5 31 18:41 ..
+      -rw-r--r--   1 ryo  staff    0  5  7 18:11 .Carol_hidden.rb
+      -rw-r--r--   1 ryo  staff    0  5  7 18:11 .Dave_hidden.js
+      -rw-r--r--   1 ryo  staff    0  5  7 18:11 .Frank_hidden.txt
+      -rw-r--r--   1 ryo  staff    0  5  7 18:10 .alice_hidden
+      -rw-r--r--   1 ryo  staff    0  5  7 18:10 .bob_hidden.txt
+      -rw-r--r--   1 ryo  staff    0  5  7 18:11 .ellen_hidden.txt
+      -rw-r--r--   1 ryo  staff    0  5  7 18:09 Ellen.txt
+      -rw-r--r--   1 ryo  staff    0  5  7 18:10 Frank.txt
+      drwxr-xr-x   3 ryo  staff   96  5 30 15:59 alice
+      -rw-r-Sr--   1 ryo  staff    0  5  7 18:08 bob.txt
+      -rw-r--r--   1 ryo  staff    0  5  7 18:08 carol.rb
+      -rw-r--r--   1 ryo  staff    0  5  7 18:08 dave.js
+      drwxr-xr-t   2 ryo  staff   64  5  7 18:20 gim
+      
+      ./test/sample_dir/alice:
+      total 0
+      drwxr-xr-x   3 ryo  staff   96  5 30 15:59 .
+      drwxr-xr-x  15 ryo  staff  480  5 30 15:59 ..
+      -rw-r--r--   1 ryo  staff    0  5 30 15:59 alice.txt
     TEXT
 
     assert_equal(expected, result)
@@ -119,24 +119,24 @@ class LsCommandTest < Test::Unit::TestCase
   def test_ls_when_single_path_and_option_all_long_reverse
     @options = { all: true, reverse: true, long: true }
     paths = ['./test/sample_dir']
-    result = @ls_command.render(@options, paths)
+    result = @ls_command.generate(@options, paths)
     expected = <<~TEXT
-    total 0
-    drwxr-xr-t   2 ryo  staff   64  5  7 18:20 gim
-    -rw-r--r--   1 ryo  staff    0  5  7 18:08 dave.js
-    -rw-r--r--   1 ryo  staff    0  5  7 18:08 carol.rb
-    -rw-r-Sr--   1 ryo  staff    0  5  7 18:08 bob.txt
-    drwxr-xr-x   3 ryo  staff   96  5 30 15:59 alice
-    -rw-r--r--   1 ryo  staff    0  5  7 18:10 Frank.txt
-    -rw-r--r--   1 ryo  staff    0  5  7 18:09 Ellen.txt
-    -rw-r--r--   1 ryo  staff    0  5  7 18:11 .ellen_hidden.txt
-    -rw-r--r--   1 ryo  staff    0  5  7 18:10 .bob_hidden.txt
-    -rw-r--r--   1 ryo  staff    0  5  7 18:10 .alice_hidden
-    -rw-r--r--   1 ryo  staff    0  5  7 18:11 .Frank_hidden.txt
-    -rw-r--r--   1 ryo  staff    0  5  7 18:11 .Dave_hidden.js
-    -rw-r--r--   1 ryo  staff    0  5  7 18:11 .Carol_hidden.rb
-    drwxr-xr-x   4 ryo  staff  128  5 30 13:18 ..
-    drwxr-xr-x  15 ryo  staff  480  5 30 15:59 .
+      total 0
+      drwxr-xr-t   2 ryo  staff   64  5  7 18:20 gim
+      -rw-r--r--   1 ryo  staff    0  5  7 18:08 dave.js
+      -rw-r--r--   1 ryo  staff    0  5  7 18:08 carol.rb
+      -rw-r-Sr--   1 ryo  staff    0  5  7 18:08 bob.txt
+      drwxr-xr-x   3 ryo  staff   96  5 30 15:59 alice
+      -rw-r--r--   1 ryo  staff    0  5  7 18:10 Frank.txt
+      -rw-r--r--   1 ryo  staff    0  5  7 18:09 Ellen.txt
+      -rw-r--r--   1 ryo  staff    0  5  7 18:11 .ellen_hidden.txt
+      -rw-r--r--   1 ryo  staff    0  5  7 18:10 .bob_hidden.txt
+      -rw-r--r--   1 ryo  staff    0  5  7 18:10 .alice_hidden
+      -rw-r--r--   1 ryo  staff    0  5  7 18:11 .Frank_hidden.txt
+      -rw-r--r--   1 ryo  staff    0  5  7 18:11 .Dave_hidden.js
+      -rw-r--r--   1 ryo  staff    0  5  7 18:11 .Carol_hidden.rb
+      drwxr-xr-x   4 ryo  staff  128  5 31 18:41 ..
+      drwxr-xr-x  15 ryo  staff  480  5 30 15:59 .
     TEXT
 
     assert_equal(expected, result)
